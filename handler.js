4f7312cf80bfabb8a293de78e7684bc035689f8b
Alok Singh <alok@noumena.global>
2023-01-21 16:14:11 +0530

    Updated dynamodb

--- handler.js
+++ handler.js
@@ -4,9 +4,15 @@ var bodyParser = require('body-parser')
 const users = require('./routes/users');
 
 const app = express();
-app.use(bodyParser.json())
-app.use("/user",users);
+
+app.use(express.json())
+
+app.use("/users",users);
 app.get('/', (req, res) => {
     return res.status(200).json({ message: "Hello" })
-})
+});
+
+app.use((req,res,next) => {
+    return res.status(404).json({error:'Not Found'});
+});
 module.exports.handler = serverless(app);
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,47 @@
+const AWS = require('aws-sdk');
 const express = require('express');
 const router = express.Router();
-const users = [];
-router.get('/', function(req, res) {
+
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamodbClient = new AWS.DynamoDB.DocumentClient();
+
+router.get('/', async function(req, res) {
     console.log("Get User");
-    return res.status(200).json({data:users})
+    const params = {
+        TableName: USERS_TABLE
+    };
+    const result = [];
+    let items;
+    do{
+        items = await dynamodbClient.scan(params).promise();
+        items.Items.forEach((item)=> result.push(item));
+        params.ExclusiveStartKey = items.LastEvaluatedKey;
+    }while(typeof items.LastEvaluatedKey !== 'undefined')
+    return res.status(200).json({data:result})
 })
-router.post('/', function(req, res) {
+router.post('/', async function(req, res) {
     console.log("add User");
-    console.log(req.body)
-    users.push(req.body);
-    return res.status(200).json({data:users})
-})
+    const {userId, name} = req.body;
+    if(typeof userId !== 'string'){
+        res.status(400).json({error: 'UserId must be a string'});
+    }else if(typeof name !== 'string'){
+        res.status(400).json({error: 'Name must be a string'});
+    }
+
+    const params = {
+        TableName: USERS_TABLE,
+        Item:{
+            userId: userId,
+            name: name
+        }
+    };
+
+    try {
+        await dynamodbClient.put(params).promise();
+        return res.status(200).json({userId, name});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Could not create user'});
+    }
+});
 module.exports = router;
